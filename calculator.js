530c3aef892ea91b02a91abee1f12030c785dcab
Programozas Karrier <info@programozaskarrier.hu>
2021-11-08 21:27:16 +0100

    url-method switchel

--- calculator.js
+++ calculator.js
@@ -1,5 +1,35 @@
-function add(num1,num2){
 
-    return num1 +num2
+
+
+
+const http = require('http');
+
+const requestListener =function (req, res) {
+
+  switch (true) {
+    case req.url === '/' && req.method === 'GET':
+      res.setHeader('content-type', 'text/html;charset=utf-8');
+      res.writeHead(200);
+      res.end('Címlap <a href ="/bejelenkezes">Bejelentkezés</a>');
+      break;
+
+    case req.url === '/bejelentkezes' && req.method === 'GET':
+      res.setHeader('content-type', 'text/html;charset=utf-8');
+      res.writeHead(200);
+      res.end('Bejelentkezés <a href ="/">Címlap</a>');
+      break;
+
+    default:
+      res.setHeader('content-type', 'text/html;charset=utf-8');
+      res.writeHead(404);
+      res.end('Oldal nem található');
+  }
+
 }
-module.exports = add;
+
+
+
+
+const server = http.createServer(requestListener);
+
+server.listen(8080)
--- index.js
+++ index.js
@@ -2,6 +2,36 @@
 
 
 
-const add = require('./calculator.js');
+const http = require('http');
 
-console.log(add(2,3));
+const requestListener =function (req, res) {
+
+  switch (true) {
+    case req.url === '/' && req.method === 'GET':
+      res.setHeader('content-type', 'text/html;charset=utf-8');
+      res.writeHead(200);
+      res.end('Ami itt látható a címlap <a href ="/bejelentkezes">Ahova visz href neve pl bejelentkezés </a>');
+      break;
+
+    case req.url === '/bejelentkezes' && req.method === 'GET':
+      res.setHeader('content-type', 'text/html;charset=utf-8');
+      res.writeHead(200);
+      res.end('Bejelentkezés helye ez az oldal <a href ="/">Címlap vagy a href neve ahova visz</a>');
+      break;
+
+    default:
+      res.setHeader('content-type', 'text/html;charset=utf-8');
+      res.writeHead(404);
+      res.end('Oldal nem található');
+
+  }
+  
+console.log(res.status)
+}
+
+
+
+
+const server = http.createServer(requestListener);
+
+server.listen(8080)
